6a5bc68b5027ec89e8d6f0c4dc8fdfa699f642dc
Margarida Baptista Lourenco <mbl@worklaptop.dyndns.cern.ch>
2024-08-14 14:04:47 +0200

    Identify the  most cited papers

--- inspire_api.py
+++ inspire_api.py
@@ -1,23 +1,30 @@
 import os
 import requests
 import json
+import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
-from pyinspirehep.contrib.clone import LiteratureClone
 from utils import get_date_range
 
 
 class INSPIREHepAPI:
-    def __init__(self, record: str = "literature", q: str = "", sort: str = None, size: str = "5", fields: list[str] = []):
+    def __init__(self, record: str = "literature", q: str = "", sort: str = None, size: str = "1000", fields: list[str] = []):
         # Initialise
-        self.base_url = f"https://inspirehep.net/api/{record}"
+        self.base_url = f"https://inspirehep.net/api"
+        self.record = record
         self.headers = {
             "Accept": "application/json"  # Indicates that the client expects a JSON response
         }
+        self.set_params(q,sort,size,fields)
+    
+    def set_params(self, q: str = "", sort: str = None, size:str = None, fields: list[str] = [])-> None:
         self.q: str = q
         self.sort: str = sort
         self.size: str = size
         self.fields: list[str] = fields
+
+    def set_record(self, record: str):
+        self.record = record
 
     def set_query(self, q: str) -> None:
         self.q = q
@@ -25,22 +32,24 @@ class INSPIREHepAPI:
     def set_fields(self, fields: list[str]) -> None:
         self.fields = fields
 
-    def search_papers(self) -> dict:
-        """
-        Search for papers based on the query.
-        """
+    def __fetch(self)-> dict:
         params = {
             'q': self.q,
             'sort': self.sort,
             'size': self.size,
         }
-        
-        response = requests.get(self.base_url, headers=self.headers, params=params)
+    
+        response = requests.get(url=f'{self.base_url}/{self.record}', headers=self.headers, params=params)
         print(response.url)
         if response.status_code == 200:
             return response.json()
         else:
             response.raise_for_status()
+    
+    def get_paper(self, dois: str) -> dict:
+        query :str = f'dois.value:{dois}'
+        self.set_params(q=query)
+        return self.__fetch()
 
     def extract_references_dois(self, references: list[dict]) -> list[str]:
         """
@@ -52,12 +61,12 @@ class INSPIREHepAPI:
         Returns:
             List of DOIs found in all references.
         """
-        dois_list = []
+        dois_set: set[str] = set()
         for reference in references:
             dois = self.extract_nested_field(reference, 'reference.dois')
             if dois:
-                dois_list.extend(dois)
-        return dois_list
+                dois_set.update(dois)
+        return dois_set
 
     def extract_nested_field(self, data: dict, field: str) -> any:
         """
@@ -85,7 +94,7 @@ class INSPIREHepAPI:
         """
         Get specific data fields for papers based on the query.
         """
-        data = self.search_papers()
+        data = self.__fetch()
         results = []
         for paper in data['hits']['hits']:
             metadata = paper['metadata']
@@ -104,17 +113,41 @@ class INSPIREHepAPI:
 
 if __name__ == "__main__":
 
-    fields = ['titles.title','references.reference.dois'
-              ,'publication_info.artid','dois.value','authors.recid', 'imprints.date','citation_count']
+    fields = ['titles.title','dois.value', 'imprints.date',
+              'citation_count', 'references.reference.dois','authors.recid']
+    #'publication_info.artid'
     api = INSPIREHepAPI(fields=fields)
     # sort-order: mostrecent, mostcited
     # Construct the query
     start_date_str, end_date_str = get_date_range(days = 90)
     print(start_date_str)
     print(end_date_str)
-    query = f"date:[{start_date_str} TO {end_date_str}]"
+    query = f"date:[{start_date_str} TO {end_date_str}]&citation_count_without_self_citations"
     print(query)
     api.set_query(query)
     data: list[dict[str]] = api.get_data()
-    #for i in range(0, len(data)):
-        #print(data[i])
+    print(len(data))
+    ref_list: list[str] = []
+    for paper in data:
+        if paper.get("references.reference.dois") is not None:
+            #print(f'LEN: {len(paper["references.reference.dois"])}')
+            ref_list.extend(paper["references.reference.dois"])
+        #else:
+            #print('LEN: 0')
+    #print(f'LENGTH: {len(ref_list)}\n {ref_list}')
+    
+    series = pd.Series(ref_list).value_counts()
+    print(series)
+    print(f'LENGTH: {len(ref_list)}')
+    # Limit to top 5 categories
+    top_n = 15
+    limited_series = series.nlargest(top_n).sort_values(ascending=True)
+    limited_series.plot.barh()
+    plt.title(f'Top {top_n} Citations')
+
+    # Adjust layout to show full y labels
+    plt.tight_layout()
+    plt.show()
+    
+    # REPEATING PAPERS!
+ 
